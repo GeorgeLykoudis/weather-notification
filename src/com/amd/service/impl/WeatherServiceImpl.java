8116f3e4281b9bdc4d0aed7add94505cb389d5c6
GiwrgosLykoudis <glykoudis92@gmail.com>
2021-04-29 16:00:48 +0300

    added implementation of weather service

--- src/com/amd/service/impl/WeatherServiceImpl.java
+++ src/com/amd/service/impl/WeatherServiceImpl.java
@@ -2,6 +2,11 @@ package com.amd.service.impl;
 
 import com.amd.service.WeatherService;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 /**
@@ -25,6 +30,19 @@ public class WeatherServiceImpl implements WeatherService {
      */
     @Override
     public HttpResponse<String> getWeatherTemperature() {
-        return null;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return response;
     }
 }
